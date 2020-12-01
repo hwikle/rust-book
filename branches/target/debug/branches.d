2514a0c9da451909470b301587c4e9797415ca5e
Hank Wikle <henry.wikle@gmail.com>
2020-12-01 16:37:43 -0700

    Add branches example program

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/branches/target/debug/branches: /home/hank/Documents/programming/rust/tutorials/rust-book/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-4d7ac4619e13a24a.d
@@ -0,0 +1,5 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/branches/target/debug/deps/branches-4d7ac4619e13a24a: src/main.rs
+
+/home/hank/Documents/programming/rust/tutorials/rust-book/branches/target/debug/deps/branches-4d7ac4619e13a24a.d: src/main.rs
+
+src/main.rs:
