62d2b147f3702b864fc0f6078cb1513646e1d183
Hank Wikle <henry.wikle@gmail.com>
2020-12-01 16:00:31 -0700

    Add functions example program

---
+++ functions/target/debug/deps/functions-5898ca96dcae1e3a.d
@@ -0,0 +1,5 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/functions/target/debug/deps/functions-5898ca96dcae1e3a: src/main.rs
+
+/home/hank/Documents/programming/rust/tutorials/rust-book/functions/target/debug/deps/functions-5898ca96dcae1e3a.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/functions/target/debug/functions: /home/hank/Documents/programming/rust/tutorials/rust-book/functions/src/main.rs
