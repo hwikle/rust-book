3d75ea49ecee55c5b3f07d05cf6d9765ddfb608b
Hank Wikle <henry.wikle@gmail.com>
2020-12-01 20:01:05 -0700

    Add ownership example program

---
+++ ownership/target/debug/deps/ownership-52bb36939743d87e.d
@@ -0,0 +1,5 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/ownership/target/debug/deps/ownership-52bb36939743d87e: src/main.rs
+
+/home/hank/Documents/programming/rust/tutorials/rust-book/ownership/target/debug/deps/ownership-52bb36939743d87e.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/ownership/target/debug/ownership: /home/hank/Documents/programming/rust/tutorials/rust-book/ownership/src/main.rs
