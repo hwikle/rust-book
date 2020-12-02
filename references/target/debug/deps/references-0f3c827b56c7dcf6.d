9f26ce50f1437592088716314e72d8e7936b9bd3
Hank Wikle <henry.wikle@gmail.com>
2020-12-02 08:47:59 -0700

    Add references example program

---
+++ references/target/debug/deps/references-0f3c827b56c7dcf6.d
@@ -0,0 +1,5 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/references/target/debug/deps/references-0f3c827b56c7dcf6: src/main.rs
+
+/home/hank/Documents/programming/rust/tutorials/rust-book/references/target/debug/deps/references-0f3c827b56c7dcf6.d: src/main.rs
+
+src/main.rs:
---
+++ references/target/debug/references.d
@@ -0,0 +1,1 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/references/target/debug/references: /home/hank/Documents/programming/rust/tutorials/rust-book/references/src/main.rs
