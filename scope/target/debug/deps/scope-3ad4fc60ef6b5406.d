b850418d0df4297614d17b1e3b14349b925faa4d
Hank Wikle <henry.wikle@gmail.com>
2020-12-01 19:55:16 -0700

    Add scope example program

---
+++ scope/target/debug/deps/scope-3ad4fc60ef6b5406.d
@@ -0,0 +1,5 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/scope/target/debug/deps/scope-3ad4fc60ef6b5406: src/main.rs
+
+/home/hank/Documents/programming/rust/tutorials/rust-book/scope/target/debug/deps/scope-3ad4fc60ef6b5406.d: src/main.rs
+
+src/main.rs:
---
+++ scope/target/debug/scope.d
@@ -0,0 +1,1 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/scope/target/debug/scope: /home/hank/Documents/programming/rust/tutorials/rust-book/scope/src/main.rs
