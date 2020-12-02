40de88d4d8f038bd9c455aeb749f9fbb78349aef
Hank Wikle <henry.wikle@gmail.com>
2020-12-01 20:09:24 -0700

    Add return values scope example program

---
+++ return_values/target/debug/deps/return_values-01d253702c557f35.d
@@ -0,0 +1,5 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/return_values/target/debug/deps/return_values-01d253702c557f35: src/main.rs
+
+/home/hank/Documents/programming/rust/tutorials/rust-book/return_values/target/debug/deps/return_values-01d253702c557f35.d: src/main.rs
+
+src/main.rs:
---
+++ return_values/target/debug/return_values.d
@@ -0,0 +1,1 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/return_values/target/debug/return_values: /home/hank/Documents/programming/rust/tutorials/rust-book/return_values/src/main.rs
