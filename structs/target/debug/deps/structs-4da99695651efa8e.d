73773b8454a0acd94444765260ce8e9c5bc6b40a
Hank Wikle <henry.wikle@gmail.com>
2020-12-02 11:26:26 -0700

    Add structs example program

---
+++ structs/target/debug/deps/structs-4da99695651efa8e.d
@@ -0,0 +1,5 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/structs/target/debug/deps/structs-4da99695651efa8e: src/main.rs
+
+/home/hank/Documents/programming/rust/tutorials/rust-book/structs/target/debug/deps/structs-4da99695651efa8e.d: src/main.rs
+
+src/main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/structs/target/debug/structs: /home/hank/Documents/programming/rust/tutorials/rust-book/structs/src/main.rs
