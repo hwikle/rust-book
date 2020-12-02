eb86218938bc779cc7bab0930673c73b7d9103e1
Hank Wikle <henry.wikle@gmail.com>
2020-12-02 11:08:11 -0700

    Add slices example program

---
+++ slices/target/debug/deps/slices-d5e6f6e3920ebe0c.d
@@ -0,0 +1,5 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/slices/target/debug/deps/slices-d5e6f6e3920ebe0c: src/main.rs
+
+/home/hank/Documents/programming/rust/tutorials/rust-book/slices/target/debug/deps/slices-d5e6f6e3920ebe0c.d: src/main.rs
+
+src/main.rs:
---
+++ slices/target/debug/slices.d
@@ -0,0 +1,1 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/slices/target/debug/slices: /home/hank/Documents/programming/rust/tutorials/rust-book/slices/src/main.rs
