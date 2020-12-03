3d44a6a4acdf99e8cc9591eec076f240515a6edf
Hank Wikle <henry.wikle@gmail.com>
2020-12-03 10:50:13 -0700

    Add rectangles example program

---
+++ rectangles/target/debug/deps/rectangles-7f8afbefd74d1f94.d
@@ -0,0 +1,5 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/rectangles/target/debug/deps/rectangles-7f8afbefd74d1f94: src/main.rs
+
+/home/hank/Documents/programming/rust/tutorials/rust-book/rectangles/target/debug/deps/rectangles-7f8afbefd74d1f94.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/hank/Documents/programming/rust/tutorials/rust-book/rectangles/target/debug/rectangles: /home/hank/Documents/programming/rust/tutorials/rust-book/rectangles/src/main.rs
